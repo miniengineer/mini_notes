cdf8e96cca69bdf1fe5d0d0f8ea6bf206f9a3f20
Minira Samadova <samadova.minira@gmail.com>
2019-12-20 11:22:41 +0900

    fixed bug of PATCH request for first note

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -14,30 +14,32 @@ class App extends React.Component {
     super();
     this.state = {
       isLoggedIn: false,
-      currentUser: {
-        id: 2,
-        username: 'Fabian Prewett',
-        email: 'fabian.p@gmail.com'
-      },
+      currentUser: null,
       selectedNoteIndex: null,
       selectedNote: null,
       miniNotes: null
     }
   }
 
-  componentDidMount = () => {
-    axios.get(`/api/v1/users/${this.state.currentUser.id}/notes`).then(response => this.setState({ miniNotes: response.data.data }));
-  }
+  // ///////////Get all notes for the current user
+  // componentDidMount = () => {
+  //   this.getAllMiniNotes();
+  // }
+
+
+  // ///////////API call to get all notes for the current user from DB
+  // getAllMiniNotes = async () => {
+  //   await axios.get(`/api/v1/users/${this.state.currentUser.id}/notes`).then(response => this.setState({ miniNotes: response.data.data }));
+  // }
 
   selectNote = (note, index) => this.setState({ selectedNoteIndex: index, selectedNote: note });
 
   noteUpdate = async (id, noteObj) => {
     //update note in the db
     const updatedNote = await axios.patch(`/api/v1/users/${this.state.currentUser.id}/notes/${id}`, noteObj);
-    console.log(updatedNote);
     const updatedNoteList = [...this.state.miniNotes];
     updatedNoteList[this.state.miniNotes.indexOf(this.state.miniNotes.find(note => note.id === updatedNote.data.data.id))] = updatedNote.data.data;
-    this.setState({ miniNotes: updatedNoteList }, () => console.log(this.state.miniNotes));
+    this.setState({ miniNotes: updatedNoteList });
   }
 
   newMiniNote = async (title) => {
@@ -48,6 +50,7 @@ class App extends React.Component {
     };
     //add this note to the db
     const newID = await axios.post(`/api/v1/users/${this.state.currentUser.id}/notes`, miniNote);
+    miniNote.id = newID.data.data.id;
     //replace currently selected note with the one just created
     await this.setState({ miniNotes: [...this.state.miniNotes, miniNote] });
     const newMiniNoteIndex = this.state.miniNotes.indexOf(this.state.miniNotes.filter(note => note.id === newID.data.data.id)[0]);
@@ -73,15 +76,18 @@ class App extends React.Component {
     this.setState({ miniNotes: afterDeletion.data.data });
   }
 
-    //successfull google login
+    ////////////////////Get all notes for current user using logged-in info
     onSuccessResponce = async (responce) => {
       const username = responce.profileObj.givenName;
       const email = responce.profileObj.email;
       const resp = await axios.get('/api/v1/users');
+      //////Check whether user has an account
       const currentUser = resp.data.data.find(user => user.email === email);
       if (currentUser) {
-        this.setState({ currentUser });
-        axios.get(`/api/v1/users/${this.state.currentUser.id}/notes`).then(response => this.setState({ miniNotes: response.data.data, isLoggedIn: true }));
+        await this.setState({ currentUser });
+        const miniNotesFromDb = await axios.get(`/api/v1/users/${this.state.currentUser.id}/notes`);
+        this.setState({ miniNotes: miniNotesFromDb.data.data, isLoggedIn: true });
+        // .then(response => this.setState({ miniNotes: response.data.data, isLoggedIn: true }))
       } else {
         const response = await axios.post('/api/v1/users', { username, email });
         const newUser = response.data.data.find(user => user.email === email);
@@ -89,19 +95,22 @@ class App extends React.Component {
       }
     };
 
+    //////////////////Failed google login
     onFailureResponce = (responce) => {
       console.log(responce);
     };
 
-    //logout from google account
+    /////////////////////////Logout from google account
     logout = () => {
       console.log("logout success");
       this.setState({ isLoggedIn: false});
     }
 
+    ///////////////////////////Failed google logout
     onFailure = () => {
       console.log("failed to logout");
     }
+
 
   render() {
 
